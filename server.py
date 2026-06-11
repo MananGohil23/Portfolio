d44bcd9a10edb348332fa5550f7b7cfed63ffc28
Manan Gohil <manangohil2357@gmail.com>
2026-06-11 19:39:19 +0530

    added local database

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request , redirect
+import csv
+
 app = Flask(__name__)
 
 
@@ -6,7 +8,30 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+# def write_to_file(data):
+#     with open('database.txt', mode='a') as database:
+#         email = data['email']
+#         subject = data['subject']
+#         message = data['message']
+#         file = database.write(f'\n{email},{subject},{message}')
+
+def write_to_csv(data):
+    with open('database.csv', mode='a', newline='') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database , delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
 
 @app.route('/<string:route>')
 def dynamic_route(route):
     return render_template(route)
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_csv(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'something went wrong. Try again!'
